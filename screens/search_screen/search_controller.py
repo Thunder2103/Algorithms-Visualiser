76e17937726fb173d559998f156b8f70c4fa41e1
Thomas Gibson <thomasgibson2003@live.com>
2024-01-28 23:51:07 +0000

    Add comments to SearchModel getters and setters

--- screens/search_screen/search_controller.py
+++ screens/search_screen/search_controller.py
@@ -143,4 +143,4 @@ class SearchController():
           
     def getMaxBars(self): return self.__maxBars
 
-    
+# Listen to Sure Enough by The Door Cinema Club     
--- screens/search_screen/search_model.py
+++ screens/search_screen/search_model.py
@@ -12,7 +12,7 @@ class SearchModel():
         self.__minPadding = 5
         # Maximum distance between displayed array and edge of canvas
         self.__maxPadding = 20
-        # Maximum number of bars that can be drawn on screen
+        # Maximum number of pixels that can be used to draw bars on screen
         self.__maximumPixels = None
         # Lowest value that could appear in the array
         self.__randomLow = 100
@@ -25,23 +25,42 @@ class SearchModel():
         # Array containing all the data 
         self.__array = []
     
-    def getBarWidth(self) -> int: return self.__barWidth
-    def setBarWidth(self, value : int) -> None: self.__barWidth = value
+    # Returns width of the bars drawn on screen
+    def getBarWidth(self) -> int: return self.__barWidth 
+    # Sets width of bars drawn on screen to passed value
+    def setBarWidth(self, value : int) -> None: self.__barWidth = value 
+    # Returns the smallest width the bars can be
     def getMinBarWidth(self) -> int: return self.__minBarWidth 
-    def getMaxBarWidth(self) -> int: return self.__maxBarWidth
-    def getBarDistance(self) -> int: return self.__barDist
-    def getMinPadding(self) -> int: return self.__minPadding
+    # Returns the largest width the bars can be
+    def getMaxBarWidth(self) -> int: return self.__maxBarWidth 
+    # Returns the distance between each bar
+    def getBarDistance(self) -> int: return self.__barDist 
+    # Returns minimum padding between the first/last bars and the edges of the canvas
+    def getMinPadding(self) -> int: return self.__minPadding 
+    # Sets the minimum distance between the first/last bars and the edges of the canvas 
+    # to the passed value
     def setMinPadding(self, value : int) -> None: self.__minPadding = value
-    def getMaxPadding(self) -> int: return self.__maxPadding
-    def getMaximumPixels(self) -> int: return self.__maximumPixels
-    def setMaximumPixels(self, value : int) -> None: self.__maximumPixels = value
+    # Returns maximium padding between the first/last bars and the edges of the canvas
+    def getMaxPadding(self) -> int: return self.__maxPadding 
+    # Returns maximum number of pixels that can be used to draw bars
+    def getMaximumPixels(self) -> int: return self.__maximumPixels 
+    # Sets maximum pixels to passed value
+    def setMaximumPixels(self, value : int) -> None: self.__maximumPixels = value 
+    # Gets the absolute lowest number that could appear in the array
     def getLowestRandomBound(self) -> int: return self.__randomLow
+    # Gets the absolute highest number that could appear in the array 
     def getHighestRandomBound(self) -> int: return self.__randomHigh 
-    def getLowerBound(self) -> int: return self.__lowerBound
-    def setLowerBound(self, value : int) -> None: self.__lowerBound = value
+    # Gets the lowest the number that can appear in the array
+    def getLowerBound(self) -> int: return self.__lowerBound 
+    # Sets the lowest number that can appear in the array to the value passed
+    def setLowerBound(self, value : int) -> None: self.__lowerBound = value 
+    # Gets the highest value that can appear in the array
     def getHigherBound(self) -> int: return self.__higherBound
+    # Set the highest value that can appear in the array to the value passed
     def setHigherBound(self, value : int) -> None: self.__higherBound = value
 
     def getArray(self) -> list: return self.__array
     def appendArray(self, value : int) -> None: self.__array.append(value)
     def popArray(self) -> None: self.__array.pop()
+
+# Listen to creep by Radiohead
